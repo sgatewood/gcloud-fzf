bf114a843cb09db76b34fb6526f0668d701bc4e9
sgatewood <stg2bd@virginia.edu>
2025-02-16 16:04:31 -0500

    support `--version`

--- main.go
+++ main.go
@@ -9,10 +9,17 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
+var (
+	Version   = "dev"
+	Commit    = "none"
+	BuildDate = "unknown"
+)
+
 func main() {
 	cmd := &cli.Command{
-		Name:  "boom",
-		Usage: "make an explosive entrance",
+		Name:    "gcloud-fzf",
+		Usage:   "runs the spotlight",
+		Version: Version,
 		Action: func(ctx context.Context, cmd *cli.Command) error {
 			err := spotlight.Spotlight(ctx)
 			if err != nil {
